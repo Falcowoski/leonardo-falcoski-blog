b4d3ff9adbea9a58fadd10e812f4f6b4934f50e4
Leonardo Falcoski <leonardo.falcoski@gmail.com>
2026-02-17 21:06:35 -0300

    fix: Fix TSC warnings

--- src/consts.ts
+++ src/consts.ts
@@ -4,8 +4,6 @@ export const SITE: Site = {
   NAME: "Leonardo Falcoski",
   EMAIL: "leonardo.falcoski@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
